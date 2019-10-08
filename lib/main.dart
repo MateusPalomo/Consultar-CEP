677db11262ab61bedc23bd2c275452fc3be4565a
Mateus <mateus.mgo@hotmail.com>
2019-10-08 20:17:02 -0300

    Consulta de CEP

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:search_cep/views/home_page.dart';
+import 'package:dynamic_theme/dynamic_theme.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DynamicTheme(
+        defaultBrightness: Brightness.light,
+        data: (brightness) => ThemeData(
+              primarySwatch: Colors.amber,
+              accentColor: Colors.amber,
+              brightness: brightness,
+            ),
+        themedWidgetBuilder: (context, theme) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Flutter Demo',
+            theme: theme,
+            home: HomePage(),
+          );
+        });
+  }
+}
